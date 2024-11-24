e53b4c6660e712fb985d3127a571192b36610775
shayaan68 <158122854+shayaan68@users.noreply.github.com>
2024-11-24 16:23:46 -0500

    doing some corrections

--- Assignment 4 - Intro to java script/Part 1/main.js
+++ Assignment 4 - Intro to java script/Part 1/main.js
@@ -9,21 +9,19 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-2. RAW TEXT STRINGS
+storytext = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
 
-It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+insertX ="Willy the Goblin"
+"Big Daddy"
+"Father Christmas"
 
-Willy the Goblin
-Big Daddy
-Father Christmas
+insertY = "the soup kitchen"
+"Disneyland"
+"the White House"
 
-the soup kitchen
-Disneyland
-the White House
-
-spontaneously combusted
-melted into a puddle on the sidewalk
-turned into a slug and crawled away
+insertZ = "spontaneously combusted"
+"melted into a puddle on the sidewalk"
+"turned into a slug and crawled away"
 
 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
